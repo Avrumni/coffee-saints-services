8a9c33f9e89506fb39096f69736706ccbccf17ad
Travis CI <dev@motome.com.au>
2017-09-29 17:19:36 +1000

    Refactor buyer controller to use response object

--- src/main/java/au/com/coffeesaints/saints/buyer/BuyerController.java
+++ src/main/java/au/com/coffeesaints/saints/buyer/BuyerController.java
@@ -1,32 +1,40 @@
 package au.com.coffeesaints.saints.buyer;
 
 import au.com.coffeesaints.saints.SaintEntity;
+import au.com.coffeesaints.saints.SaintMapper;
+import au.com.coffeesaints.saints.SaintResponse;
 import au.com.coffeesaints.saints.SaintService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Log4j
 public class BuyerController {
 
-    @Autowired
-    private BuyerService buyerService;
+    private final BuyerService buyerService;
+    private final SaintService saintService;
 
     @Autowired
-    private SaintService saintService;
+    public BuyerController(BuyerService buyerService, SaintService saintService) {
+        this.buyerService = buyerService;
+        this.saintService = saintService;
+    }
 
     @GetMapping("/coffee-groups/{coffeeGroupId}/saints/buying")
-    public SaintEntity saintBuyer(@PathVariable Integer coffeeGroupId, @RequestParam List<Integer> saintIds) throws Exception {
-        return buyerService.findBuyer(coffeeGroupId, saintIds);
+    public SaintResponse saintBuyer(@PathVariable Integer coffeeGroupId, @RequestParam List<Integer> saintIds) throws Exception {
+        return SaintMapper.map(buyerService.findBuyer(coffeeGroupId, saintIds));
     }
 
     @PostMapping("/coffee-groups/{coffeeGroupId}/saints/{buyerId}/buying")
-    public List<SaintEntity> buyingSaint(@PathVariable Integer coffeeGroupId, @PathVariable int buyerId, @RequestBody List<Integer> consumerIds) throws Exception {
+    public List<SaintResponse> buyingSaint(@PathVariable Integer coffeeGroupId, @PathVariable int buyerId, @RequestBody List<Integer> consumerIds) throws Exception {
         buyerService.buyingSaint(coffeeGroupId, buyerId, consumerIds);
-        return saintService.findAllInCoffeeGroupAndInSaintIds(coffeeGroupId, consumerIds);
+        return saintService.findAllInCoffeeGroupAndInSaintIds(coffeeGroupId, consumerIds).stream()
+            .map(SaintMapper::map)
+            .collect(Collectors.toList());
     }
 
     @ExceptionHandler(Exception.class)
